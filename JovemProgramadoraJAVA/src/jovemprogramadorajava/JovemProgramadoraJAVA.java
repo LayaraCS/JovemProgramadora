98a649f88350211fb5d6001cebfae6e034a13d5e
LayaraCS <laycampossant@gmail.com>
2025-10-08 21:34:26 -0300

    Aula 1

--- JovemProgramadoraJAVA/src/jovemprogramadorajava/JovemProgramadoraJAVA.java
+++ JovemProgramadoraJAVA/src/jovemprogramadorajava/JovemProgramadoraJAVA.java
@@ -5,7 +5,27 @@ package jovemprogramadorajava;
 public class JovemProgramadoraJAVA {
 
     
+   
+    
+    
     public static void main(String[] args) {
+       System.out.println("Ola Mundo");
+        String nome = "Layara";
+        String sobrenome = "Campos Santana";
+        
+        int idade;
+        int anoDeNascimento = 1996, anoAtual = 2025;
+        
+        idade = anoAtual - anoDeNascimento;
+        
+        double altura = 1.65;
+        boolean temCNH = false;
+        char sexo = 'M';
+       System.out.println("Eu me chamo " + nome + " " + sobrenome + " e tenho " + idade + " anos");
+        System.out.println("Você tem " + idade + " anos");
+               
+               
+               
        
     }
     
